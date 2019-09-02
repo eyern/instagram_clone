233b763a9a12ea9ee595cf0b7fb3080ff307e239
eyern <eyernfuture@gmail.com>
2019-09-02 16:50:38 +0300

    including all the dependencies for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn instagram.wsgi
+web: gunicorn your_project_name.wsgi --log-file -
--- instagram/settings.py
+++ instagram/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
